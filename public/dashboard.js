e0cd9cf7a0a687eb58af7b98f183e74ac1f771e0
Jim <jimbotech007@gmail.com>
2024-02-22 17:26:03 +1100

    Split over several html/js files and added service code page

---
+++ public/dashboard.js
@@ -0,0 +1,74 @@
+
+import { auth, setUser, getClinics } from './firebase.js';
+import { islogoutButtonPressed, resetlogoutButtonPressed, showLoginScreen, showUser } from './footer.js';
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        setUser(user);
+        //const details = JSON.stringify(user, null, '  ');
+        //alert(`${details}`)
+        showUser(user)
+        initClinics();
+    } else {
+        // User is signed out
+        if (islogoutButtonPressed()) {
+            resetlogoutButtonPressed();
+        } else {
+            alert("Please sign in first")
+        }
+        showLoginScreen()
+    }
+});
+
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown");
+    if (dropdown.classList.contains("hidden")) {
+        dropdown.classList.remove("hidden");
+    } else {
+        dropdown.classList.add("hidden");
+    }
+}
+
+clinicSetup.addEventListener('click', () => {
+    window.location.href = '/clinics.html';
+});
+serviceCodes.addEventListener('click', () => {
+    window.location.href = '/serviceCode.html';
+});
+
+// Get a reference to the dropdown
+var dropdown = document.getElementById('dropdown');
+
+async function initClinics() {
+
+    getClinics().then((clinics) => {
+        if (clinics) {
+            addHeader("Please select clinic");
+            populateClinic(clinics);
+        } else {
+            dropdown.classList.add("hidden");
+        }
+    })
+}
+
+function addHeader(headerTxt) {
+    // Create a new h2 element
+    var h2 = document.createElement('h2');
+    h2.textContent = headerTxt;
+    // Get the div to which you want to add the h2
+    var div = document.getElementById('selectClinic');
+    // Add the h2 to the div
+    div.insertBefore(h2, div.firstChild);
+}
+
+function populateClinic(clinics) {
+    for (const [index, clinic] of clinics.entries()) {
+        const option = document.createElement('option');
+        const entry = clinic.clinicName;
+        option.textContent = entry
+        //option.href = `clinic.html?clinic=${index}`;
+        dropdown.appendChild(option);
+    }
+    dropdown.classList.remove("hidden");
+}
---
+++ public/firebase.js
@@ -0,0 +1,73 @@
+//
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import {
+    getAuth, connectAuthEmulator, createUserWithEmailAndPassword,
+    signInWithEmailAndPassword, onAuthStateChanged, signOut
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+const firebaseConfig = {
+    apiKey: "AIzaSyAmDuJ0yEyvQDVVApiYcqAgp7yaOJkudgA",
+    authDomain: "drjim-f2087.firebaseapp.com",
+    projectId: "drjim-f2087",
+    storageBucket: "drjim-f2087.appspot.com",
+    messagingSenderId: "13689300459",
+    appId: "1:13689300459:web:f71baf9ca9eb861cdda5ae",
+    measurementId: "G-EE0ZDV3F0X"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+connectAuthEmulator(auth, "http://localhost:9099");
+export let currentUser = null
+export function setUser(user) {
+    currentUser = user
+}
+
+/////////////////////////////////////////////////////////////
+import {
+    connectFirestoreEmulator,
+    getFirestore,
+    setDoc,
+    doc,
+    getDoc,
+    updateDoc,
+    collection,
+    addDoc
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js'
+
+export const db = getFirestore(app);
+connectFirestoreEmulator(db, 'localhost', 8080)
+
+export async function getClinics() {
+    const userId = currentUser.uid;
+    const docRef = doc(db, userId, "clinics");
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        return docSnap.data().clinics;
+    }
+    return null;
+}
+///////////////////////////////////////////////////////////////
+// import {
+//     connectStorageEmulator,
+//     getStorage,
+//     ref,
+//     uploadString
+// } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js'
+//const storage = getStorage(app)
+//connectStorageEmulator(storage, '127.0.0.1', 9199)
+// function uploadFile() {
+//     try {
+//         const storageRef = ref(storage, `user/${currentUser.uid}/test.txt`)
+//         const content = `Hello, World! by ${currentUser.email} on ${new Date().toISOString()}`
+//         uploadString(storageRef, content).then((snapshot) => {
+//             console.log('File uploaded')
+//         }).catch((error) => {
+//             alert(error.message)
+//         });
+//         writeToFirestore()
+//     } catch (error) {
+//         alert(error.message)
+//     }
+// }
