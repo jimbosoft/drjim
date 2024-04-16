56fdba09d1e34f29cc122db87518348db4d50658
Jim <jimbotech007@gmail.com>
2024-04-16 14:56:43 +1000

    Should accept blank address for the time being

--- public/clinics.js
+++ public/clinics.js
@@ -116,7 +116,7 @@ function addDeleteButtonHandler(deleteButton, index) {
 const form = document.getElementById('company-form');
 form.addEventListener('input', (e) => {
     // If the target of the event is an address input
-    if (e.target.classList.contains('address')) {
+    if (e.target.classList.contains('companyName')) {
         // Get the parent section of the input
         const section = e.target.parentElement;
 
@@ -139,7 +139,7 @@ submitButton.addEventListener('click', async (e) => {
     let companiesArray = companies.map(company => ({
         id: company.docId,
         name: company.name,
-        address: company.address
+        address: company.address || ""
     }));
 
     const userId = currentUser.uid;
