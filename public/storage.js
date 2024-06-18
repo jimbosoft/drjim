56be22361ffadb0d67cea5401b35776afbc5d66f
Jim <jimbotech007@gmail.com>
2024-06-18 17:12:50 +1000

    separated localStorage out

--- public/storage.js
+++ public/storage.js
@@ -1,45 +1,10 @@
+
 export let currentUser = null
 export const userKey = 'user'
 export function setUser(user) {
-    //
-    // Invalid input
-    //
-    if (!user || user == 'null' || user == 'undefined') {
-        console.log('setUser called with invalid user')
-        return null
-    }
-    //
-    // Set user if none is set
-    //
-    let u = localStorage.getItem(userKey);
-    if (u && u !== 'null' && u !== 'undefined') {
-         u = JSON.parse(u);
-        //
-        // New user
-        //
-        if (u.email !== user.email) {
-            console.log('User changed!')
-            return null
-        }
-    }
-    localStorage.setItem(userKey, JSON.stringify(user));
-    currentUser = user
-    return user
+    return currentUser = user;
 }
 
-export function getUser() {
-    let u = localStorage.getItem(userKey);
-    if (u && u !== 'null' && u !== 'undefined') {
-        currentUser = JSON.parse(u);
-        return currentUser
-    }
-    return null
-}
-
-export function clearUser() {
-    localStorage.removeItem(userKey);
-    currentUser = null
-}
 export const clinicId = 'clinicId';
 
 export function setClinicId(id) {
